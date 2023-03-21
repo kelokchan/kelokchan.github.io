4548f94458648386614bfd3f0cfa4ba8776aff06
Kelok Chan <kelokchan@gmail.com>
2023-03-21 10:14:08 +0800

    let's just stick with pure html for now

--- script/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  window.onload = function () {
-    document.body.style.opacity = 1;
-  };
-
-  document.getElementById('year').innerHTML = new Date().getFullYear();
-})();
